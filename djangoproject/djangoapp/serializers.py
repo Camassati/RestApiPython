70ef94773596563ce01b7b76e62fd7883069d642
Gustavo Martins <gfirminocruz@gmail.com>
2024-08-02 23:13:22 -0300

    Rest Api Python

---
+++ djangoproject/djangoapp/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializer 
+from .models import User
+
+class UserSerializer(serializer.ModelSerializer):
+    class Meta:
+        model = User
+        fields ='__all__'
